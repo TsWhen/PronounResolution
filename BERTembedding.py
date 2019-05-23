0870ab129d12cbbcd11f73c8f1c29707502b0416
TsWhen <jiahaohuangjh@gmail.com>
2019-05-23 21:08:52 +0800

    update

--- BERTembedding.py
+++ BERTembedding.py
@@ -5,4 +5,38 @@ import pandas as pd
 from bert import tokenization,extract_features,modeling
 
 
+def get_offset_no_spaces(gap_text,offset):
+
+    count = 0
+    for pos in range(offset):
+
+        if gap_text[pos] != " ":
+            count += 1
+
+    return count
+
+def get_count_chars_no_special(gap_text):
+
+    count = 0
+    special_char_list = ["#"]
+    for pos in range(len(gap_text)):
+
+        if gap_text[pos] not in special_char_list:
+
+            count += 1
+
+    return count
+
+
+def get_count_lenght_no_special(gap_text):
+
+    count = 0;
+
+    special_char_list = ["#"," "]
+    for pos in range(len(gap_text)):
+
+        if gap_text[pos] not in special_char_list:
+            count += 1
+
+    return count
 
